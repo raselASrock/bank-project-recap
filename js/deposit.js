d1ec761d30e81045843650dd398270fbeda7b666
raselASrock <mdraselm413@gmail.com>
2022-08-17 18:13:41 +0600

    js-file-added

---
+++ js/deposit.js
@@ -0,0 +1,10 @@
+/*
+1.  Add event Listener to the diposit Button
+2. Get deposit amount from the deposit input field.
+*/ 
+
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    console.log('Deposit Button Clicked')
+
+    const depositField = document.getElementById('deposit-field');
+})
