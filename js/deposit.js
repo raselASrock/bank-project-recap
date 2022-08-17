2c74c1a27242f8f86a093301cf8bcc4e98764a5f
raselASrock <mdraselm413@gmail.com>
2022-08-17 20:23:41 +0600

    withdraw-file-added

--- js/deposit.js
+++ js/deposit.js
@@ -5,6 +5,8 @@
 3. Clear the deposit field after getting the value.
 4. Get the previous deposit total.
 5. Calculate new deposit total and set the value to the deposit total.
+6. Get current balance total.
+7. Calculate the new balance and set it to the balance total element.
 */ 
 
 
@@ -45,6 +47,17 @@ const newDepositTotal =  previousDepositTotal + newDepositAmount;
 
 depositTotalElement.innerText = newDepositTotal;
 
+// Step:6****************
+
+const balanceTotalElement = document.getElementById('balance-total');
+
+const previousBalanceTotalString = balanceTotalElement.innerText;
+const previousBalanceTotalAmount = parseFloat(previousBalanceTotalString);
+
+// Step:7*************
+
+const newBalanceTotal = previousBalanceTotalAmount + newDepositAmount;
+balanceTotalElement.innerText = newBalanceTotal;
 
 
 })
