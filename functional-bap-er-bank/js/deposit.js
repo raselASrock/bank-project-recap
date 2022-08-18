1967af3ee648cbe822f2b82800508be41aaaab59
raselASrock <mdraselm413@gmail.com>
2022-08-18 19:41:31 +0600

    diposit-function-added

--- functional-bap-er-bank/js/deposit.js
+++ functional-bap-er-bank/js/deposit.js
@@ -1,3 +1,30 @@
+/*
+1. Get the element by id
+2. Get the value from the element
+3. Convert string value to a number
+
+*/ 
+
+function getInputFieldValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId)
+    const inputFieldValueString = inputField.value ;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = ''
+    return inputFieldValue;
+}
+
+function getElementValueById(elementId){
+const element = document.getElementById(elementId)
+const elementValueString = element.innerText;
+const value = parseFloat(elementValueString);
+return value;
+}
+
 document.getElementById('btn-deposit').addEventListener('click', function(){
     console.log('Deposit Button Clicked');
+
+
+const newDepositAmount = getInputFieldValueById('deposit-field')
+
+
 })
