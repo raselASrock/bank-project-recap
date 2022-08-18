ce329786a46bbb3cdaab6c80f6146d126be7cc9e
raselASrock <mdraselm413@gmail.com>
2022-08-18 16:36:31 +0600

    new-code-added

---
+++ js/withdraw.js
@@ -0,0 +1,15 @@
+/*
+1. Add event handler with the withdraw button
+2. Get the withdraw amount
+
+*/ 
+
+// Step: 1***************
+
+document.getElementById('btn-withdeaw').addEventListener('click', function(){
+
+
+    // Step:2************
+    
+
+})
