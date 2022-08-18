ca7d78ef7b6dcf210078d4fc8bb354c325866659
raselASrock <mdraselm413@gmail.com>
2022-08-18 17:02:39 +0600

    second-code

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,6 +1,9 @@
 /*
 1. Add event handler with the withdraw button
 2. Get the withdraw amount
+3. Clear the withdraw input field
+4. Get previous Withdraw total
+5. Calculate Total. and set it to the withdraw total element
 
 */ 
 
@@ -10,6 +13,23 @@ document.getElementById('btn-withdeaw').addEventListener('click', function(){
 
 
     // Step:2************
+    const withdrawField = document.getElementById('withdraw-field')
+    const newWithdrawAmountString = withdrawField.value ;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    // Step: 3********************
+
+    withdrawField.value = ''
+
+    // Step: 4*****************
+
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTptal = parseFloat(previousWithdrawTotalString);
+
+    // Step: 5*********************
+
     
 
 })
