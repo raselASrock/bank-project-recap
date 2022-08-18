5e572c4828b3226f5314f58719de68e3812ee8a5
raselASrock <mdraselm413@gmail.com>
2022-08-18 20:17:08 +0600

    function-added

--- functional-bap-er-bank/js/deposit.js
+++ functional-bap-er-bank/js/deposit.js
@@ -14,17 +14,32 @@ function getInputFieldValueById(inputFieldId){
 }
 
 function getElementValueById(elementId){
-const element = document.getElementById(elementId)
-const elementValueString = element.innerText;
-const value = parseFloat(elementValueString);
-return value;
+const textElement = document.getElementById(elementId)
+const textElementValueString = textElement.innerText;
+const textElementValue = parseFloat(textElementValueString);
+return textElementValue;
 }
 
+function setTextElementValueById ()
+
+
+
 document.getElementById('btn-deposit').addEventListener('click', function(){
-    console.log('Deposit Button Clicked');
+    // console.log('Deposit Button Clicked');
 
 
 const newDepositAmount = getInputFieldValueById('deposit-field')
+/*
+1. get previous deposit total by id
 
+
+*/ 
+
+const previousDepositTotal = getTextElementValueById('deposit-total')
+
+// Calculate new deposit total 
+
+const newDepositTotal = previousDepositTotal + newDepositAmount ;
+balanceTotalElement.innerText = newBalanceTotal;
 
 })
